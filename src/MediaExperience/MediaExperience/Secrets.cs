a0ac4ca9eface386e2c784c4791c8658581ec43c
Andrey Zakharov <mediaexplorer74@hotmail.com>
2022-08-22 00:13:52 +0300

    .

--- src/MediaExperience/MediaExperience/Secrets.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-namespace MediaExperience
-{
-    public static class Secrets
-    {
-        public const string AppId = ""; //get an app ID from https://apps.dev.microsoft.com/
-	}
-}
--- src/Microsoft.ConnectedDevices.iOS/ConnectedDevices.framework/Headers/MCDCommon.h
+++
@@ -1,5 +0,0 @@
-//
-//  Copyright (c) Microsoft Corporation. All rights reserved.
-//
-
-#define __MCD_VISIBLE_EXTERNALLY __attribute__((visibility("default")))
